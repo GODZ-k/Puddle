46acc1af98fb9bd6bd20a678c7be8b1d02daa652
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-04 22:40:07 +0530

    add availability

--- items/models.py
+++ items/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
     description=models.TextField(blank=True, null=True)
     price=models.FloatField()
     image=models.ImageField(upload_to="item_images" , null=True, blank=True)
-    is_sold=models.BooleanField(default=False)
+    sold_out=models.BooleanField(default=False)
     created_by=models.ForeignKey(User, related_name="items" , on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
 
