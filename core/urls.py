783cada66323b6636472d6384f78cb2bad2d0338
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-08 13:20:38 +0530

    final user authentication

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from .views import *
 from items.views import *
 urlpatterns = [
     path("", home , name="/"),
+    path("logout/",logout_user,name="logout"),
     path("login/",login_user,name="login_user"),
-    path("signup/",signup_user,name="signup_user")
+    path("signup/",signup_user,name="signup_user"),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from items.models import Catagories,Item
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here
 
@@ -14,7 +18,48 @@ def home(request):
     return render(request, "index.html",data)
 
 def login_user(request):
+    if request.method == "POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+
+        user_data=User.objects.filter(username=email)
+        if not user_data.exists():
+            messages.error(request,"Please enter valid email")
+            return redirect("/login/")
+        else:
+            user=authenticate(username=email, password=password)
+            if not user:
+                messages.error(request,"Please enter valid password")
+                return redirect("/login/")
+            else:
+                login(request,user)
+                return redirect("/")
     return render(request,'login.html')
 
 def signup_user(request):
+    if request.method == 'POST':
+        name=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+
+
+        user_email=User.objects.filter(username=email)
+        if user_email.exists():
+            messages.error(request,"User already exists")
+            return redirect("/signup/")
+
+        user=User.objects.create(
+            username=email,
+            first_name=name
+            # email=email
+         )
+        user.set_password(password)
+        user.save()
+        messages.success(request,"Account created successfully")
+        return redirect("/login/")
+
     return render(request,'signup.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect("/login/")
