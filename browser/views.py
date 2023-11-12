11684da37b1d945f9af840b16190bde3b6109503
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-12 12:03:55 +0530

    category filter

--- browser/views.py
+++ browser/views.py
@@ -7,20 +7,26 @@ from django.db.models import Q
 def index(request):
     items=Item.objects.filter(sold_out=False)
     category=Catagories.objects.all()
+
     if request.method == 'GET':
         search=request.GET.get('search','')
+        category_name=request.GET.get('category', '')
+
         if search != None:
             items=Item.objects.filter(
                 Q(name__icontains=search)|
                 Q(price__icontains=search)|
-                Q(description__icontains=search)|
-                Q(catagories__name__icontains=search)
+                Q(description__icontains=search)
+                # Q(categories__name__icontains=search)
             )
+        if category_name:
+         items=items.filter(catagories__name=category_name)
 
     data={
         "items": items,
         "search":search,
-        "category": category
+        "category": category,
+        "category_name":category_name
         # "item":item
     }
     return render(request,'browser.html',data)
